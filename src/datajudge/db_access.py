ec5dc6135983ee9eefb276c74cabd895ac3f35c5
Yasin <102247161+YasinTatarQC@users.noreply.github.com>
2022-05-27 17:01:31 +0200

    Clean if statement
    Co-authored-by: Ignacio Vergara Kausel <ivergarakausel@gmail.com>

--- src/datajudge/db_access.py
+++ src/datajudge/db_access.py
@@ -649,7 +649,7 @@ def _column(engine, ref, aggregate_operator: Callable = None):
         selection = sa.select([column])
         result = engine.connect().execute(selection).all()
 
-        return [elem[0] for elem in result], [selection]
+        result =  [elem[0] for elem in result]
 
     else:
         selection = sa.select([aggregate_operator(column)])
