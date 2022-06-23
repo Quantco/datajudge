8c7f25a970b89302d9dcf0081246de097926642e
Yasin Tatar <yasin.tatar@quantco.com>
2022-06-23 18:12:53 +0200

    integrate sql query with data refs

--- src/datajudge/db_access.py
+++ src/datajudge/db_access.py
@@ -909,31 +909,25 @@ def get_ks_2sample(engine: sa.engine.Engine, ref: DataReference, ref2: DataRefer
     Runs the query for the two-sample Kolmogorov-Smirnov test and returns the test statistic d.
     """
 
-    tab1, col1 = None, None
-    tab2, col2 = None, None
-
+    tab1, col1 = str(ref.get_selection(engine)), ref.get_column(engine)
+    tab2, col2 = str(ref2.get_selection(engine)), ref2.get_column(engine)
     ks_query_string = f"""
-        WITH cdf_unfilled AS (
-            -- Step 1: Calculate the CDF for both tables and join them together
-            SELECT coalesce(tab1.val, tab2.val) as v, tab1.cdf as cdf1, tab2.cdf as cdf2
-            FROM
-            (
-                SELECT val, MAX(cdf) as cdf FROM (
-                    SELECT val, cume_dist() over (order by val) as cdf
-                    FROM (SELECT {col1} as val FROM {tab1}) -- Change TABLE and COL here
-                    ORDER BY val
-                ) GROUP BY val
-            ) as tab1
-            FULL OUTER JOIN
-            (
-                SELECT val, MAX(cdf) as cdf FROM (
-                    SELECT val, cume_dist() over (order by val) as cdf
-                    FROM (SELECT {col2} as val FROM {tab2}) -- Change TABLE and COL here
-                    ORDER BY val
-                ) GROUP BY val
-            ) as tab2
-            ON tab1.val = tab2.val
-            ORDER BY v
+        WITH tab1 AS (
+        SELECT val, MAX(cdf) as cdf FROM (
+            SELECT val, cume_dist() over (order by val) as cdf
+            FROM (SELECT {col1} as val FROM ({tab1})) -- Change TABLE and COL here
+            ORDER BY val
+        ) GROUP BY val
+        ), tab2 AS (
+        SELECT val, MAX(cdf) as cdf FROM (
+            SELECT val, cume_dist() over (order by val) as cdf
+            FROM (SELECT {col2} as val FROM ({tab2})) -- Change TABLE and COL here
+            ORDER BY val
+        ) GROUP BY val
+        ), cdf_unfilled AS (
+        SELECT coalesce(tab1.val, tab2.val) as v, tab1.cdf as cdf1, tab2.cdf as cdf2
+        FROM tab1 FULL OUTER JOIN tab2 ON tab1.val = tab2.val
+        ORDER BY v
         ), grouped_table AS ( -- Step 2: Create a grouper attribute to fill values forward
             SELECT v,
                 COUNT(cdf1) over (order by v) as _grp1, cdf1,
@@ -942,8 +936,12 @@ def get_ks_2sample(engine: sa.engine.Engine, ref: DataReference, ref2: DataRefer
         ), filled_cdf AS ( -- Step 3: Select first non-null value per group (defined by grouper)
             SELECT v, first_value(cdf1) over (partition by _grp1 order by v) as cdf1_filled,
                     first_value(cdf2) over (partition by _grp2 order by v) as cdf2_filled
-        FROM grouped_table
-        ) SELECT * FROM filled_cdf;
+            FROM grouped_table
+        ), replaced_nulls AS ( -- Step 4: Replace NULL values (often at the begin) with 0 to calculate difference
+        SELECT coalesce(cdf1_filled, 0) as cdf1, coalesce(cdf2_filled, 0) as cdf2
+        FROM filled_cdf
+        )
+        SELECT MAX(ABS(cdf1-cdf2)) FROM replaced_nulls; -- Step 5: Calculate final statistic
     """
 
     d_statisitic = engine.execute(ks_query_string).scalar()
