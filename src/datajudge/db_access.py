989dc9940094eba8002f231a39cb03fbfdbb3698
Yasin Tatar <yasin.tatar@quantco.com>
2022-06-28 14:15:33 +0200

    better formatting for understandibility

--- src/datajudge/db_access.py
+++ src/datajudge/db_access.py
@@ -923,43 +923,51 @@ def get_ks_2sample(engine: sa.engine.Engine, table1: tuple, table2: tuple):
         table1_selection = f"({table1_selection})"
         table2_selection = f"({table2_selection})"
 
+    # for a more extensive explanation, see:
+    # https://github.com/Quantco/datajudge/pull/28#issuecomment-1165587929
     ks_query_string = f"""
-        WITH tab1 AS (SELECT val, MAX(cdf) as cdf
-                      FROM (SELECT val, cume_dist() over (order by val) as cdf
-                            FROM (SELECT {col1} as val
-                                  FROM {table1_selection} as temp01) as temp03 -- Change TABLE and COL here
-                            -- ORDER BY val -- removed, because invalid in MS-SQL Server
-                            ) as temp05
-                      GROUP BY val),
-             tab2 AS (SELECT val, MAX(cdf) as cdf
-                      FROM (SELECT val, cume_dist() over (order by val) as cdf
-                            FROM (SELECT {col2} as val
-                                  FROM {table2_selection} as temp02) as temp04 -- Change TABLE and COL here
-                            -- ORDER BY val -- removed, because invalid in MS-SQL Server
-                            ) as temp06
-                      GROUP BY val),
-             cdf_unfilled AS (SELECT coalesce(tab1.val, tab2.val) as v, tab1.cdf as cdf1, tab2.cdf as cdf2
-                              FROM tab1
-                                       FULL OUTER JOIN tab2 ON tab1.val = tab2.val
-                              -- ORDER BY v -- removed, because invalid in MS-SQL Server
-                              ),
-             grouped_table AS ( -- Step 2: Create a grouper attribute to fill values forward
-                 SELECT v,
-                        COUNT(cdf1) over (order by v) as _grp1,
-                        cdf1,
-                        COUNT(cdf2) over (order by v) as _grp2,
-                        cdf2
-                 FROM cdf_unfilled),
-             filled_cdf AS ( -- Step 3: Select first non-null value per group (defined by grouper)
-                 SELECT v,
-                        first_value(cdf1) over (partition by _grp1 order by v) as cdf1_filled,
-                        first_value(cdf2) over (partition by _grp2 order by v) as cdf2_filled
-                 FROM grouped_table),
-             replaced_nulls AS ( -- Step 4: Replace NULL values (often at the begin) with 0 to calculate difference
-                 SELECT coalesce(cdf1_filled, 0) as cdf1, coalesce(cdf2_filled, 0) as cdf2
-                 FROM filled_cdf)
-        SELECT MAX(ABS(cdf1 - cdf2))
-        FROM replaced_nulls; -- Step 5: Calculate final statistic
+        WITH
+        tab1 AS (
+            SELECT val, MAX(cdf) as cdf
+            FROM (SELECT val, cume_dist() over (order by val) as cdf
+                FROM (SELECT {col1} as val
+                      FROM {table1_selection} as temp01) as temp03 -- Change TABLE and COL here
+                ) as temp05
+            GROUP BY val
+        ),
+        tab2 AS (
+            SELECT val, MAX(cdf) as cdf
+            FROM (SELECT val, cume_dist() over (order by val) as cdf
+                FROM (SELECT {col2} as val
+                      FROM {table2_selection} as temp02) as temp04 -- Change TABLE and COL here
+                ) as temp06
+            GROUP BY val
+        ),
+        cdf_unfilled AS (
+            SELECT coalesce(tab1.val, tab2.val) as v, tab1.cdf as cdf1, tab2.cdf as cdf2
+            FROM tab1 FULL OUTER JOIN tab2 ON tab1.val = tab2.val
+        ),
+        -- Step 2: Create a grouper attribute to fill values forward
+        grouped_table AS (
+            SELECT v,
+                COUNT(cdf1) over (order by v) as _grp1,
+                cdf1,
+                COUNT(cdf2) over (order by v) as _grp2,
+                cdf2
+            FROM cdf_unfilled
+        ),
+        -- Step 3: Select first non-null value per group (defined by grouper)
+        filled_cdf AS (
+            SELECT v,
+                first_value(cdf1) over (partition by _grp1 order by v) as cdf1_filled,
+                first_value(cdf2) over (partition by _grp2 order by v) as cdf2_filled
+            FROM grouped_table),
+        -- Step 4: Replace NULL values (often at the begin) with 0 to calculate difference
+        replaced_nulls AS (
+            SELECT coalesce(cdf1_filled, 0) as cdf1, coalesce(cdf2_filled, 0) as cdf2
+            FROM filled_cdf)
+        -- Step 5: Calculate final statistic as max. distance
+        SELECT MAX(ABS(cdf1 - cdf2)) FROM replaced_nulls;
     """
 
     d_statistic = engine.execute(ks_query_string).scalar()
