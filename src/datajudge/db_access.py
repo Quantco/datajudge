ddb6441343a938aeba1feb2f440ff993fc1b45df
kklein <kleinkevin.lu@gmail.com>
2022-07-28 17:39:13 +0200

    Bluntly translate raw string to sqlalchemy language expresions.

--- src/datajudge/db_access.py
+++ src/datajudge/db_access.py
@@ -913,65 +913,80 @@ def get_ks_2sample(
     Runs the query for the two-sample Kolmogorov-Smirnov test and returns the test statistic d.
     """
     # For mssql: "tempdb.dbo".table_name -> tempdb.dbo.table_name
-    table1_str = str(ref1.data_source.get_clause(engine)).replace('"', "")
     col1 = ref1.get_column(engine)
-    table2_str = str(ref2.data_source.get_clause(engine)).replace('"', "")
     col2 = ref2.get_column(engine)
 
-    # for a more extensive explanation, see:
-    # https://github.com/Quantco/datajudge/pull/28#issuecomment-1165587929
-    ks_query_string = f"""
-        WITH
-        tab1 AS ( -- Step 0: Prepare data source and value column
-            SELECT {col1} as val FROM {table1_str}
-        ),
-        tab2 AS (
-            SELECT {col2} as val FROM {table2_str}
-        ),
-        tab1_cdf AS ( -- Step 1: Calculate the CDF over the value column
-            SELECT val, cume_dist() over (order by val) as cdf
-            FROM tab1
-        ),
-        tab2_cdf AS (
-            SELECT val, cume_dist() over (order by val) as cdf
-            FROM tab2
-        ),
-        tab1_grouped AS ( -- Step 2: Remove unnecessary values, s.t. we have (x, cdf(x)) rows only
-            SELECT val, MAX(cdf) as cdf
-            FROM tab1_cdf
-            GROUP BY val
-        ),
-        tab2_grouped AS (
-            SELECT val, MAX(cdf) as cdf
-            FROM tab2_cdf
-            GROUP BY val
-        ),
-        joined_cdf AS ( -- Step 3: combine the cdfs
-            SELECT coalesce(tab1_grouped.val, tab2_grouped.val) as v, tab1_grouped.cdf as cdf1, tab2_grouped.cdf as cdf2
-            FROM tab1_grouped FULL OUTER JOIN tab2_grouped ON tab1_grouped.val = tab2_grouped.val
-        ),
-        -- Step 4: Create a grouper id based on the value count; this is just a helper for forward-filling
-        grouped_cdf AS (
-            SELECT v,
-                COUNT(cdf1) over (order by v) as _grp1,
-                cdf1,
-                COUNT(cdf2) over (order by v) as _grp2,
-                cdf2
-            FROM joined_cdf
-        ),
-        -- Step 5: Forward-Filling: Select first non-null value per group (defined in the prev. step)
-        filled_cdf AS (
-            SELECT v,
-                first_value(cdf1) over (partition by _grp1 order by v) as cdf1_filled,
-                first_value(cdf2) over (partition by _grp2 order by v) as cdf2_filled
-            FROM grouped_cdf),
-        -- Step 6: Replace NULL values (at the beginning) with 0 to calculate difference
-        replaced_nulls AS (
-            SELECT coalesce(cdf1_filled, 0) as cdf1, coalesce(cdf2_filled, 0) as cdf2
-            FROM filled_cdf)
-        -- Step 7: Calculate final statistic as max. distance
-        SELECT MAX(ABS(cdf1 - cdf2)) FROM replaced_nulls;
-    """
+    selection1 = ref1.get_selection(engine)
+    selection2 = ref2.get_selection(engine)
 
-    d_statistic = engine.execute(ks_query_string).scalar()
+    s1 = sa.select(
+        [
+            selection1.selected_columns[col1].label("val"),
+            sa.func.cume_dist().over(order_by=col1).label("cdf"),
+        ]
+    ).subquery()
+    s2 = sa.select(
+        [
+            selection2.selected_columns[col2].label("val"),
+            sa.func.cume_dist().over(order_by=col2).label("cdf"),
+        ]
+    ).subquery()
+
+    t1 = (
+        sa.select([s1.c["val"], sa.func.max(s1.c["cdf"]).label("cdf")])
+        .group_by(s1.c["val"])
+        .subquery()
+    )
+    t2 = (
+        sa.select([s2.c["val"], sa.func.max(s2.c["cdf"]).label("cdf")])
+        .group_by(s2.c["val"])
+        .subquery()
+    )
+
+    join = (
+        sa.select(
+            sa.func.coalesce(t1.c["val"], t2.c["val"]).label("v"),
+            t1.c["cdf"].label("cdf1"),
+            t2.c["cdf"].label("cdf2"),
+        )
+        .select_from(t1.join(t2, t1.c["val"] == t2.c["val"], isouter=True, full=True))
+        .subquery()
+    )
+
+    grouped_cdf = sa.select(
+        [
+            join.c["v"],
+            sa.func.count(join.c["cdf1"]).over(order_by=join.c["v"]).label("_grp1"),
+            join.c["cdf1"],
+            sa.func.count(join.c["cdf2"]).over(order_by=join.c["v"]).label("_grp2"),
+            join.c["cdf2"],
+        ]
+    ).subquery()
+
+    filled_cdf = sa.select(
+        [
+            grouped_cdf.c["v"],
+            sa.func.first_value(grouped_cdf.c["cdf1"])
+            .over(partition_by=grouped_cdf.c["_grp1"], order_by=grouped_cdf.c["v"])
+            .label("cdf1_filled"),
+            sa.func.first_value(grouped_cdf.c["cdf2"])
+            .over(partition_by=grouped_cdf.c["_grp2"], order_by=grouped_cdf.c["v"])
+            .label("cdf2_filled"),
+        ]
+    ).subquery()
+
+    replaced_nulls = sa.select(
+        [
+            sa.func.coalesce(filled_cdf.c["cdf1_filled"], 0).label("cdf1"),
+            sa.func.coalesce(filled_cdf.c["cdf2_filled"], 0).label("cdf2"),
+        ]
+    ).subquery()
+
+    final_selection = sa.select(
+        sa.func.max(sa.func.abs(replaced_nulls.c["cdf1"] - replaced_nulls.c["cdf2"]))
+    )
+
+    with engine.connect() as connection:
+        d_statistic = connection.execute(final_selection).scalar()
+
     return d_statistic
