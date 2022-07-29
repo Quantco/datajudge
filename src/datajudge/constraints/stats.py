61eb97ec86cf4de7553d0ad553db84b36a7d9a18
kklein <kleinkevin.lu@gmail.com>
2022-07-29 18:56:29 +0200

    Use type annotations as if it was Python 3.8 days.

--- src/datajudge/constraints/stats.py
+++ src/datajudge/constraints/stats.py
@@ -1,6 +1,6 @@
 import math
 import warnings
-from typing import Optional
+from typing import List, Optional, Tuple
 
 import sqlalchemy as sa
 
@@ -73,7 +73,7 @@ class KolmogorovSmirnov2Sample(Constraint):
         engine,
         ref1: DataReference,
         ref2: DataReference,
-    ) -> tuple[float, Optional[float], int, int, list]:
+    ) -> Tuple[float, Optional[float], int, int, List]:
 
         # retrieve test statistic d, as well as sample sizes m and n
         d_statistic, ks_selections = db_access.get_ks_2sample(
