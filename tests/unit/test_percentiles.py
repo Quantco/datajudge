1eb72b45504ef6299d24eadb76452c3a31845c8a
kklein <kleinkevin.lu@gmail.com>
2023-03-12 20:20:13 +0100

    Move variable to fixture.

--- tests/unit/test_percentiles.py
+++ tests/unit/test_percentiles.py
@@ -3,12 +3,15 @@ import pytest
 from datajudge.constraints.numeric import NumericPercentile
 from datajudge.db_access import DataReference, TableDataSource
 
-data_source = TableDataSource("my_db", "my_table")
-ref = DataReference(data_source, ["column"], None)
+
+@pytest.fixture(scope="module")
+def ref():
+    data_source = TableDataSource("my_db", "my_table")
+    return DataReference(data_source, ["column"], None)
 
 
 @pytest.mark.parametrize("k", [-0.5, 100.5])
-def test_invalid_k(k):
+def test_invalid_k(ref, k):
     with pytest.raises(ValueError):
         NumericPercentile(ref, k, 0, 0)
 
@@ -24,6 +27,6 @@ def test_invalid_k(k):
         (None, -1),
     ],
 )
-def test_invalid_deviations(max_absolute_deviation, max_relative_deviation):
+def test_invalid_deviations(ref, max_absolute_deviation, max_relative_deviation):
     with pytest.raises(ValueError):
         NumericPercentile(ref, 50, max_absolute_deviation, max_relative_deviation)
