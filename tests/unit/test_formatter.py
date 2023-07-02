c0591c3479354896ec708bf4bbb68c9c11288195
Bela Stoyan <bela.stoyan@quantco.com>
2023-07-02 22:39:37 +0200

    remove HTMLFormatter Test

--- tests/unit/test_formatter.py
+++ tests/unit/test_formatter.py
@@ -1,6 +1,6 @@
 from colorama import Back
 
-from datajudge.formatter import AnsiColorFormatter, Formatter, HtmlFormatter
+from datajudge.formatter import AnsiColorFormatter, Formatter
 
 
 def test_default_formatter():
@@ -19,18 +19,5 @@ def test_ansi_color_formatter():
         formatter.fmt_str("[numDiff]Hello[/numDiff]") == f"{Back.CYAN}Hello{Back.RESET}"
     )
     assert formatter.fmt_str("[numMatch]Hello[/numMatch]") == "Hello"
-    assert formatter.fmt_str("[b]Hello[/b]") == "[b]Hello[/b]"
-    assert formatter.fmt_str("[numDiff]Hello[/numMatch]") == "[numDiff]Hello[/numMatch]"
-
-
-def test_html_formatter():
-    formatter = HtmlFormatter()
-
-    assert formatter.fmt_str("[numDiff]Hello[/numDiff]") == (
-        "<span style='background-color: #FF0000; color: #FFFFFF'>Hello</span>"
-    )
-    assert formatter.fmt_str("[numMatch]Hello[/numMatch]") == (
-        "<span style='background-color: #00FF00; color: #FFFFFF'>Hello</span>"
-    )
     assert formatter.fmt_str("[b]Hello[/b]") == "[b]Hello[/b]"
     assert formatter.fmt_str("[numDiff]Hello[/numMatch]") == "[numDiff]Hello[/numMatch]"
