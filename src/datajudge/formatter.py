2f63acbb7b3fe40a2270598e7d47dd446d100ed6
kklein <kevin.klein@quantco.com>
2026-01-15 18:37:39 +0100

    Fix inheritance

--- src/datajudge/formatter.py
+++ src/datajudge/formatter.py
@@ -4,21 +4,21 @@ import re
 from colorama import Back, just_fix_windows_console
 
 # example: match = [numMatch]...[/numMatch]
-STYLING_CODES = r"\[(numMatch|numDiff)\](.*?)\[/\1\]"
+_STYLING_CODES = r"\[(numMatch|numDiff)\](.*?)\[/\1\]"
 
 
 class Formatter(abc.ABC):
     def __init__(self):
-        self.known_bb_pattern = re.compile(STYLING_CODES)
+        self._known_bb_pattern = re.compile(_STYLING_CODES)
 
     # Just ignore styling in the default formatter
-    def apply_formatting(self, _: str, inner: str) -> str:
+    def _apply_formatting(self, style: str, inner: str) -> str:
         return inner
 
     def fmt_str(self, string: str) -> str:
         # Replace codes with platform specific styling
-        string = self.known_bb_pattern.sub(
-            lambda m: self.apply_formatting(m.group(1), m.group(2)), string
+        string = self._known_bb_pattern.sub(
+            lambda m: self._apply_formatting(m.group(1), m.group(2)), string
         )
 
         return string
@@ -29,8 +29,8 @@ class AnsiColorFormatter(Formatter):
         super().__init__()
         just_fix_windows_console()
 
-    def apply_formatting(self, code: str, inner: str) -> str:
-        if code == "numDiff":
+    def _apply_formatting(self, style: str, inner: str) -> str:
+        if style == "numDiff":
             return f"{Back.CYAN}{inner}{Back.RESET}"
         else:
             return inner
